ab49868baefe95f0aea312dab23d83c1c48f5bc1
Jorge Septien <Jorge.Septien@cubic.com>
2019-07-09 13:44:35 -0700

    P3 sorting

--- P2/problem_3.py
+++ P2/problem_3.py
@@ -9,43 +9,73 @@ for e.g. [1, 2, 3, 4, 5]
 The expected answer would be [531, 42]. Another expected answer can be [542, 31]. In scenarios such as these when there are more than one possible answers, return any one.
 
 '''
+def mergesort(items):
+
+    if len(items) <= 1:
+        return items
+
+    mid = len(items) // 2
+    left = items[:mid]
+    right = items[mid:]
+
+    left = mergesort(left)
+    right = mergesort(right)
+
+    return merge(left, right)
+
+
+def merge(left, right):
+
+    merged = []
+    left_index = 0
+    right_index = 0
+
+    while left_index < len(left) and right_index < len(right):
+        if left[left_index] > right[right_index]:
+            merged.append(right[right_index])
+            right_index += 1
+        else:
+            merged.append(left[left_index])
+            left_index += 1
+
+    merged += left[left_index:]
+    merged += right[right_index:]
+
+    return merged
+
 
 def rearrange_digits(input_list):
-    """
-    Rearrange Array Elements so as to form two number such that their sum is maximum.
-
-    Args:
-       input_list(list): Input List
-    Returns:
-       (int),(int): Two maximum sums
-    """
-    sorted_nums = sorted(input_list)
-    print('sorted {}'.format(sorted_nums))
-    solution = [0, 0]
+    sorted_digits = mergesort(input_list)
+    num1 = 0
+    num2 = 0
     index = 0
     multiplier = 1
-    while index < len(sorted_nums):
-        print('index {}, solution {}'.format(index, solution))
-        solution[0] += sorted_nums[index]*multiplier
-        if index+1 < len(sorted_nums):
-            solution[1] += sorted_nums[index+1]*multiplier
+    while index < len(sorted_digits):
+        num1 += sorted_digits[index]*multiplier
+        if index+1 < len(sorted_digits):
+            num2 += sorted_digits[index+1]*multiplier
         index += 2
         multiplier *= 10
-    print('sum is {}'.format(solution[0] + solution[1]))
-    return solution
-
+    return (num1, num2)
 
 
 def test_function(test_case):
     output = rearrange_digits(test_case[0])
     solution = test_case[1]
-    print('solution {}, output {}'.format(solution, output))
     if sum(output) == sum(solution):
         print("Pass")
     else:
         print("Fail")
 
-test_function([[1, 2, 3, 4, 5], [542, 31]])
+
+test_case = [[1, 2, 3, 4, 5], [542, 31]]
+test_function(test_case)
 
 test_case = [[4, 6, 2, 5, 9, 8], [964, 852]]
+test_function(test_case)
+
+test_case = [[4, 6], [4, 6]]
+test_function(test_case)
+
+test_case = [[4], [4, 0]]
 test_function(test_case)
